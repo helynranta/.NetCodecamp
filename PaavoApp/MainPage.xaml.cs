2b18da06aaf0f29d3453e11836bf0c171fff0955
Nathorr <nathorr@gmail.com>
2013-12-11 15:55:13 +0200

    RuutCam
    Toimii lel

--- PaavoApp/MainPage.xaml.cs
+++ PaavoApp/MainPage.xaml.cs
@@ -18,50 +18,54 @@ using System.Windows.Media.Imaging;
 
 namespace PaavoApp
 {
-    public partial class MainPage : PhoneApplicationPage
-    {
-        // Constructor
-        public MainPage()
-        {
-            InitializeComponent();
+	public partial class MainPage : PhoneApplicationPage
+	{
+		// Constructor
+		public MainPage()
+		{
+			InitializeComponent();
 
-            // Set the data context of the listbox control to the sample data
-            DataContext = App.ViewModel;
-            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
-        }
+			// Set the data context of the listbox control to the sample data
+			DataContext = App.ViewModel;
+			this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+		}
 
-        // Load data for the ViewModel Items
-        private void MainPage_Loaded(object sender, RoutedEventArgs e)
-        {
-            if (!App.ViewModel.IsDataLoaded)
-            {
-                App.ViewModel.LoadData();
-            }
-        }
+		// Load data for the ViewModel Items
+		private void MainPage_Loaded(object sender, RoutedEventArgs e)
+		{
+			if (!App.ViewModel.IsDataLoaded)
+			{
+				App.ViewModel.LoadData();
+			}
+		}
 
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
+		private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
 
-        }
+		}
 
-        private void AalefTap(object sender, GestureEventArgs e)
-        {
-            NavigationService.Navigate(new Uri("/StudentUnion.xaml", UriKind.Relative));
-        }
+		private void AalefTap(object sender, GestureEventArgs e)
+		{
+			NavigationService.Navigate(new Uri("/StudentUnion.xaml", UriKind.Relative));
+		}
 
-        private void KurniekkaTap(object sender, GestureEventArgs e)
-        {
-            NavigationService.Navigate(new Uri("/Tentit.xaml", UriKind.Relative));
-        }
+		private void KurniekkaTap(object sender, GestureEventArgs e)
+		{
+			NavigationService.Navigate(new Uri("/Tentit.xaml", UriKind.Relative));
+		}
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            NavigationService.Navigate(new Uri("/Weather.xaml", UriKind.Relative));
-        }
-        private void ExamButtonClick(object sender, RoutedEventArgs e)
-        {
-            NavigationService.Navigate(new Uri("/Tentit.xaml", UriKind.Relative));
-        }
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			NavigationService.Navigate(new Uri("/Weather.xaml", UriKind.Relative));
+		}
+		private void ExamButtonClick(object sender, RoutedEventArgs e)
+		{
+			NavigationService.Navigate(new Uri("/Tentit.xaml", UriKind.Relative));
+		}
 
-    }
+		private void RuutButtonClick(object sender, RoutedEventArgs e)
+		{
+			NavigationService.Navigate(new Uri("/Ruut.xaml", UriKind.Relative));
+		}
+	}
 }
