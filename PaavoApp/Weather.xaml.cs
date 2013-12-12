2bb7d6db7c353314c489823b9c38daa3356cd374
Lassi Lääti <lassilaaati@gmail.com>
2013-12-12 15:39:25 +0200

    lataa lämpötilatietoja

--- PaavoApp/Weather.xaml.cs
+++ PaavoApp/Weather.xaml.cs
@@ -20,108 +20,77 @@ namespace PaavoApp
         public Weather()
         {
             InitializeComponent();
+            downloadContent("http://ilmatieteenlaitos.fi/saa/lappeenranta/skinnarila");
         }
-        // Click handler which runs when the 'Load Feed' or 'Refresh Feed' button is clicked. 
-        private void loadFeedButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        public void downloadContent(string url)
         {
-            // WebClient is used instead of HttpWebRequest in this code sample because 
-            // the implementation is simpler and easier to use, and we do not need to use 
-            // advanced functionality that HttpWebRequest provides, such as the ability to send headers.
-            WebClient webClient = new WebClient();
 
-            // Subscribe to the DownloadStringCompleted event prior to downloading the RSS feed.
-            webClient.DownloadStringCompleted += new DownloadStringCompletedEventHandler(webClient_DownloadStringCompleted);
+            WebClient wc = new WebClient();
+            wc.OpenReadCompleted += new OpenReadCompletedEventHandler(OpenReadCompleted);
+            wc.OpenReadAsync(new Uri(url), wc);
 
-            // Download the RSS feed. DownloadStringAsync was used instead of OpenStreamAsync because we do not need 
-            // to leave a stream open, and we will not need to worry about closing the channel. 
-            webClient.DownloadStringAsync(new System.Uri("http://open.live.bbc.co.uk/weather/feeds/en/648900/3dayforecast.rss"));
         }
-
-        // Event handler which runs after the feed is fully downloaded.
-        private void webClient_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        void OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
         {
-            if (e.Error != null)
+            if (e.Error == null && !e.Cancelled)
             {
-                Deployment.Current.Dispatcher.BeginInvoke(() =>
+                //download pdf
+                string content;
+                StreamReader reader = new StreamReader(e.Result);
+                content = reader.ReadToEnd();
+                //replace ä, ö, and degree
+                content = content.Replace("&auml;", "ä").Replace("&ouml;", "ö").Replace("&nbsp;", "").Replace("&deg;", "°");
+                //get the table of contents
+                string[] splitted = content.Split(new string[] {"<tbody>"}, StringSplitOptions.None);
+                foreach (string sentence in splitted)
                 {
-                    // Showing the exact error message is useful for debugging. In a finalized application, 
-                    // output a friendly and applicable string to the user instead. 
-                    MessageBox.Show(e.Error.Message);
-                });
+                    if (sentence.Contains("Kosteus") && sentence.Contains(">Lämpötila</span>"))
+                    {
+                        content = sentence;
+                        break;
+                    }
+                }
+                
+                Dictionary<string,string> tiedot = new Dictionary<string, string> 
+                { 
+                    {"Lämpötila", "1"},
+                    {"Kastepiste","2"},
+                    {"Puuska", "2"},
+                    {"Pilvistä","3"},
+                    {"Kosteus", "4"},
+                    {"Lounaistuulta","5"},
+                    {"Paine", "6"},
+                    {"Näkyvyys","7"} 
+                };
+                string content_temp = "";
+                foreach (string key in tiedot.Keys.ToList())
+                {
+                    splitted = content.Split(new string[] { "<span" }, StringSplitOptions.None);
+                    for (int i = 0; i < splitted.Length; i++)
+                    {
+                        if (splitted[i].Contains(key.ToString()))
+                        {
+                            content_temp = splitted[i + 1];
+                            break;
+                        }
+                    }
+                    
+                    splitted = content_temp.Split(new string[] { ">" }, StringSplitOptions.None);
+                    content_temp = splitted[1].Replace("</span", "");
+                    tiedot[key.ToString()] = content_temp;
+                }
             }
             else
             {
-                // Save the feed into the State property in case the application is tombstoned. 
-                this.State["feed"] = e.Result;
-
-                UpdateFeedList(e.Result);
+                //Either cancelled or error handle appropriately for your app  
             }
         }
 
-        // This method determines whether the user has navigated to the application after the application was tombstoned.
-        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        private void feedListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            // First, check whether the feed is already saved in the page state.
-            if (this.State.ContainsKey("feed"))
-            {
-                // Get the feed again only if the application was tombstoned, which means the ListBox will be empty.
-                // This is because the OnNavigatedTo method is also called when navigating between pages in your application.
-                // You would want to rebind only if your application was tombstoned and page state has been lost. 
-                if (feedListBox.Items.Count == 0)
-                {
-                    UpdateFeedList(State["feed"] as string);
-                }
-            }
+
         }
 
         // This method sets up the feed and binds it to our ListBox. 
-        private void UpdateFeedList(string feedXML)
-        {
-            // Load the feed into a SyndicationFeed instance
-            StringReader stringReader = new StringReader(feedXML);
-            XmlReader xmlReader = XmlReader.Create(stringReader);
-            SyndicationFeed feed = SyndicationFeed.Load(xmlReader);
-
-            // In Windows Phone OS 7.1, WebClient events are raised on the same type of thread they were called upon. 
-            // For example, if WebClient was run on a background thread, the event would be raised on the background thread. 
-            // While WebClient can raise an event on the UI thread if called from the UI thread, a best practice is to always 
-            // use the Dispatcher to update the UI. This keeps the UI thread free from heavy processing.
-            Deployment.Current.Dispatcher.BeginInvoke(() =>
-            {
-                // Bind the list of SyndicationItems to our ListBox
-                feedListBox.ItemsSource = feed.Items;
-
-                loadFeedButton.Content = "Refresh Feed";
-            });
-
-        }
-
-        // The SelectionChanged handler for the feed items 
-        private void feedListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            ListBox listBox = sender as ListBox;
-
-            if (listBox != null && listBox.SelectedItem != null)
-            {
-                // Get the SyndicationItem that was tapped.
-                SyndicationItem sItem = (SyndicationItem)listBox.SelectedItem;
-
-                // Set up the page navigation only if a link actually exists in the feed item.
-                if (sItem.Links.Count > 0)
-                {
-                    // Get the associated URI of the feed item.
-                    Uri uri = sItem.Links.FirstOrDefault().Uri;
-
-                    // Create a new WebBrowserTask Launcher to navigate to the feed item. 
-                    // An alternative solution would be to use a WebBrowser control, but WebBrowserTask is simpler to use. 
-                    WebBrowserTask webBrowserTask = new WebBrowserTask();
-                    webBrowserTask.Uri = uri;
-                    webBrowserTask.Show();
-                }
-            }
-        }
-
-
-
     }
 }
